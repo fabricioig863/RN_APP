539693641866cc74fb65fd0e449d1d2680b0612b
Fabricio Espindola <fabricio.espindola@dotgroup.com.br>
2022-11-22 14:18:45 -0300

    feat/create-global-styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React, {useEffect, useState} from 'react';
-import {Alert, Button, SafeAreaView} from 'react-native';
+import React, {useEffect, useState, useCallback, useMemo, useRef} from 'react';
+import {Button, SafeAreaView, TextInput} from 'react-native';
 import {Header} from './components/Header';
+import {TextInputStyles} from './styles.global';
 
 const App = () => {
   const [name, setName] = useState<string>('Fabricio');
@@ -13,23 +14,37 @@ const App = () => {
     } else {
       setTitle('Olá');
     }
-
-    return () => Alert.alert('OOPS', 'Component unmounted!');
   }, [name, title]);
 
-  const handlePressButton = () => {
-    setName(old => {
-      if (old === 'Fabricio') {
-        return 'Pedro';
-      }
-      return 'Fabricio';
-    });
-  };
+  console.log(`state: ${name}`);
+
+  // Com o useCallback teremos uma funçao que não irá ser atualizada sempre que atualizemos o estado.
+  // Será fixo, ele mantém uma funçao na memória para voce, evitando novas renderizações.
+
+  const handlePressButton = useCallback(() => {
+    if (name === 'Fabricio') {
+      console.log(`handlePress: ${name}`);
+      setName('Pedro');
+    } else {
+      setName('Ignácio');
+    }
+  }, [name]);
+
+  // useMemo = Retorna um valor memoizado.
+  const nameChange = useMemo(() => {
+    // return value
+    return `${name} | ${new Date()}`;
+  }, [name]);
+
+  //Refs = useRefs Ele é usado para oferecer uma referencia há um component diretamente.
+
+  const inputRef = useRef<TextInput>({} as TextInput);
 
   return (
     <SafeAreaView>
-      <Header title={title} name={name} />
-      <Button title="Change name" onPress={handlePressButton} />
+      <Header title={title} name={nameChange} />
+      <Button title="Change name" onPress={() => inputRef.current.blur()} />
+      <TextInputStyles ref={inputRef} onChangeText={text => setName(text)} />
     </SafeAreaView>
   );
 };
---
+++ src/styles.global.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+import { TextInput } from "react-native";
+
+export const TextInputStyles = styled(TextInput)`
+  padding: 20px;
+  background-color: rgba(0,0,0,0.5);
+  color: white;
+  font-size: 20px;
+`;
