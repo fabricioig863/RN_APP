419c971bf728d8987a6d5a6503257c384b3ab1d4
Fabricio Ignacio <fabricioig863@gmail.com>
2023-01-12 20:31:27 -0300

    feat/add-config-react-navigation-bottom-tabs

--- src/screen/Home/index.tsx
+++ src/screen/Home/index.tsx
@@ -1,37 +1,12 @@
 import Button from '@/components/Button';
-import React, {useState, useEffect} from 'react';
-import {Text} from 'react-native';
+import React from 'react';
 import * as S from './styles';
 
-const Home = ({navigation, route}) => {
-  const data = route.params;
-  const [errorMessage, setErrorMessage] = useState('');
-
-  console.log({data});
-
-  const handleNavigateParams = () => {
-    navigation.navigate('Onboarding', {
-      profile: {
-        name: 'Pedro henrique',
-      },
-    });
-  };
-
-  useEffect(() => {
-    if (data === undefined) {
-      setErrorMessage('Os dados nao foram carregados');
-    } else {
-      handleNavigateParams();
-    }
-  }, []);
-
+const Home = () => {
   return (
     <S.Container>
       <S.Content>
-        <Button onPress={handleNavigateParams}>Home</Button>
-        {errorMessage != '' && (
-          <Text>{`Home: ${data?.profile.selectedUser}`}</Text>
-        )}
+        <Button>Home</Button>
       </S.Content>
     </S.Container>
   );
